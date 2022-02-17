291019fcdaf525da485b690145236c4146819d78
jaynro <jaynro@gmail.com>
2022-02-16 20:22:16 -0600

    First draft of the solution

--- Knapsack/brute-force.py
+++ Knapsack/brute-force.py
@@ -1,15 +1,34 @@
-def solve_knapsack(profits, weights, capacity):
-  # TODO: Write your code here
 
+def solve_knapsack_recursive(profits, weights, capacity, index):
   # base check
 
-  # initialize p1
+  if capacity <= 0 | index >=  len(profits):
+    return 0
 
-  # Calculate P1 if needed
+  # initialize p1
+  p1=0
+
+  # Calculate P1 if needed: 
+  # If the weight of the element at current index exceeds, capacity then don;t process
+
+  currentWeight=weights[index]
+  newIndex = index +1
+  if currentWeight <= capacity:
+    currentProfit=profits[index]
+    newCapacity =capacity - currentWeight
+    p1=currentWeight +  solve_knapsack_recursive(profits,weights, newCapacity, newIndex )
 
   # Calculate  p2
+  p2 =solve_knapsack_recursive(profits,weights, capacity, newIndex )
 
   #return Max
+  return max (p1,p2)
+
+def solve_knapsack(profits, weights, capacity):
+  # Delegate it; first iteraction
+
+  solve_knapsack(profits, weights, capacity, o)
+
 
   return -1
 
