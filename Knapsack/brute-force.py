fbc0b2b02ef83139d40d77c965c04232cadea938
jaynro <jaynro@gmail.com>
2022-02-16 20:30:19 -0600

    Fixing syntaxis;  or condition

--- Knapsack/brute-force.py
+++ Knapsack/brute-force.py
@@ -2,7 +2,7 @@
 def solve_knapsack_recursive(profits, weights, capacity, index):
   # base check
 
-  if capacity <= 0 | index >=  len(profits):
+  if capacity <= 0 or index >=  len(profits):
     return 0
 
   # initialize p1
@@ -11,12 +11,13 @@ def solve_knapsack_recursive(profits, weights, capacity, index):
   # Calculate P1 if needed: 
   # If the weight of the element at current index exceeds, capacity then don;t process
 
-  currentWeight=weights[index]
+  
   newIndex = index +1
   if currentWeight <= capacity:
+    currentWeight=weights[index]
     currentProfit=profits[index]
     newCapacity =capacity - currentWeight
-    p1=currentWeight +  solve_knapsack_recursive(profits,weights, newCapacity, newIndex )
+    p1=currentProfit +  solve_knapsack_recursive(profits,weights, newCapacity, newIndex )
 
   # Calculate  p2
   p2 =solve_knapsack_recursive(profits,weights, capacity, newIndex )
@@ -27,10 +28,8 @@ def solve_knapsack_recursive(profits, weights, capacity, index):
 def solve_knapsack(profits, weights, capacity):
   # Delegate it; first iteraction
 
-  solve_knapsack(profits, weights, capacity, o)
+  return solve_knapsack_recursive(profits, weights, capacity, 0)
 
-
-  return -1
 
 def main():
   print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
