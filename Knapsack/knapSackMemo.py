7001f39074d565c564af78d620c034e058c26a8e
jaynro <jaynro@gmail.com>
2022-03-12 13:13:31 -0600

    final implementarion

--- Knapsack/knapSackMemo.py
+++ Knapsack/knapSackMemo.py
@@ -3,40 +3,37 @@ import numpy as np
 def solve_knapsack(profits, weights, capacity):
   # create a two dimensional array for Memoization, each element is initialized to '-1'
   # Capacity Index matrix
+  dp = [[-1 for x in range(capacity+1)] for y in range(len(profits))]
 
-  m=[[1,2,3],[4,5,6],[7,8,9]]
-  print (m)
-
-  oneTo9 = [x for x in range(1,10)]
-  n=[oneTo9 for y in range(3)]
-  print (n)
-
-
-
-
-
-  print("profits: ", profits)
-  print("weights: ", weights)
-  print("Capacity: ", capacity)
-  print("len(profits): " ,len(profits))
-  a1 =  ["-1" for x in range(capacity+1)]
-  print("a1: ")
-  print(a1)
-  dp = [
-      a1
-      #[-1 for x in range(capacity+1)] 
-      for y in range(len(weights))
-      ]
-
-  print ("dp: ")
-  print (dp)
-  dp2 = np.array(dp)
-  print (dp2)
   return knapsack_recursive(dp, profits, weights, capacity, 0)
 
 
 def knapsack_recursive(dp, profits, weights, capacity, currentIndex):
-    return
+
+    # base checks
+    if capacity <= 0 or currentIndex >= len(profits):
+        return 0
+    # if we have already solved a similar problem, return the result from memory
+    if dp[currentIndex][capacity] != -1:
+        return dp[currentIndex][capacity]
+
+  # recursive call after choosing the element at the currentIndex
+  # if the weight of the element at currentIndex exceeds the capacity, we
+  # shouldn't process this
+    profit1 = 0
+    if weights[currentIndex] <= capacity:
+        profit1 = profits[currentIndex] + knapsack_recursive(
+        dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1)
+
+    # recursive call after excluding the element at the currentIndex
+    profit2 = knapsack_recursive(
+        dp, profits, weights, capacity, currentIndex + 1)
+  
+    dp[currentIndex][capacity] = max(profit1, profit2)
+    
+    return dp[currentIndex][capacity]
+  
+
 def main():
     print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
 
